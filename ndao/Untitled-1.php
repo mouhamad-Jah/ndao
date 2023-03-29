77a51f170bc6f7e2efe642ce17b6ad1623b2e071
mouhamad-Jah <89473422+mouhamad-Jah@users.noreply.github.com>
2023-03-29 17:47:49 +0400

    Add files via upload

---
+++ ndao/Untitled-1.php
@@ -0,0 +1,6 @@
+<head>
+  <meta charset="UTF-8">
+  <title>My School Homepage</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="">
+</head>
